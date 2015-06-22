79f4b54fdcec1c4463b3d3632b34135d10a049c3
XuPeiYao <u0124008@nkfust.edu.tw>
2015-06-22 22:40:55 +0800

    Init

---
+++ Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Net;
+namespace TestForm {
+    public partial class Form1 : Form {
+        GlobalKeyboardHook keyhook;
+        public Form1() {
+            InitializeComponent();
+        }
+
+
+        void keyhook_KeyDown(object sender ,KeyEventArgs e) {
+            this.textBox1.AppendText("," + e.KeyCode);//將輸入按鍵的名稱印出
+            //這裡可以做些什麼...Ex:將值傳送至遠端或本地儲存
+            Label Target = (from t in VKeyboard
+                            where t.Text.ToUpper() == e.KeyCode.ToString().ToUpper()
+                            select t).FirstOrDefault();
+            if(Target != null)
+                Target.BackColor = Color.Gray;
+        }
+        void keyhook_KeyUp(object sender ,KeyEventArgs e) {
+            Label Target = (from t in VKeyboard
+                            where t.Text.ToUpper() == e.KeyCode.ToString().ToUpper()
+                            select t).FirstOrDefault();
+            if(Target != null)
+                Target.BackColor = this.BackColor;//恢復顏色
+        }
+
+
+        List<Label> VKeyboard = new List<Label>();
+
+        private void Form1_Load(object sender ,EventArgs e) {
+            #region 虛擬鍵盤顯示
+            string[][] Keyboard =new string[][]{
+                "D1,D2,D3,D4,D5,D6,D7,D8,D9,D0".Split(','),
+                "q,w,e,r,t,y,u,i,o,p".Split(','),
+                "a,s,d,f,g,h,j,k,l".Split(','),
+                "z,x,c,v,b,n,m".Split(','),
+                "space".Split(','),
+                "789".ToCharArray().Select(input=>"NumPad" + input).ToArray(),
+                "456".ToCharArray().Select(input=>"NumPad" + input).ToArray(),
+                "1230".ToCharArray().Select(input=>"NumPad" + input).ToArray(),
+            };
+
+            for(int i = 0; i < Keyboard.Length;i++ ) {
+                for(int j = 0; j < Keyboard[i].Length; j++) {
+                    Label NEW = new Label();
+                    NEW.Text = Keyboard[i][j];
+                    NEW.Width = 30;
+                    NEW.Height = 30;
+                    NEW.Left = j * 30;
+                    NEW.Top = i * 30;
+                    NEW.TextAlign = ContentAlignment.MiddleCenter;
+                    NEW.BorderStyle = BorderStyle.FixedSingle;
+                    this.Controls.Add(NEW);
+                    VKeyboard.Add(NEW);
+                }
+            }
+            #endregion
+
+            keyhook = new GlobalKeyboardHook(
+                NewAndHook: false ,//建構完物件不立刻開始監聽
+                HookAllKeys: true//監聽所有按鍵
+            );
+            keyhook.KeyDown += keyhook_KeyDown;//加入事件
+            keyhook.KeyUp += keyhook_KeyUp;
+            keyhook.Enable = true;//開始監聽
+        }
+
+        private void Form1_FormClosed(object sender ,FormClosedEventArgs e) {
+            keyhook.Enable = false;
+        }
+
+        
+    }
+}
---
+++ GlobalKeyboardHook.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestForm {
+    //修改自:https://github.com/merttasci/keylogger/blob/master/WindowsFormsApplication1/GlobalKeyBoardHook.cs
+    public class GlobalKeyboardHook {
+        #region 載入系統函式庫
+        [DllImport("user32.dll")]
+        static extern int CallNextHookEx(IntPtr hhk ,int code ,int wParam ,ref keyBoardHookStruct lParam);
+        [DllImport("user32.dll")]
+        static extern IntPtr SetWindowsHookEx(int idHook ,LLKeyboardHook callback ,IntPtr hInstance ,uint theardID);
+        [DllImport("user32.dll")]
+        static extern bool UnhookWindowsHookEx(IntPtr hInstance);
+        [DllImport("kernel32.dll")]
+        static extern IntPtr LoadLibrary(string lpFileName);
+        #endregion
+
+        #region 隱含參數
+        //宣告System Event Handler方法型別(委派)
+        delegate int LLKeyboardHook(int Code ,int wParam ,ref keyBoardHookStruct lParam);
+
+        //宣告系統回傳資料結構
+        struct keyBoardHookStruct {
+            public int vkCode;
+            public int scanCode;
+            public int flags;
+            public int time;
+            public int dwExtraInfo;
+        }
+
+        const int WH_KEYBOARD_LL = 13;
+        const int WM_KEYDOWN = 0x0100;
+        const int WM_KEYUP = 0x0101;
+        const int WM_SYSKEYDOWN = 0x0104;
+        const int WM_SYSKEYUP = 0x0105;
+        
+        LLKeyboardHook llkh;//System Event Handler
+        bool enable = false;
+        /// <summary>
+        /// Windows API Hook Id
+        /// </summary>
+        IntPtr HookTarget = IntPtr.Zero;
+        #endregion
+        
+        /// <summary>
+        /// 受監聽按鍵集合
+        /// </summary>
+        public List<Keys> HookedKeys{get;set;}
+
+        /// <summary>
+        /// 表示是否啟用掛勾
+        /// </summary>
+        public bool Enable {
+            get {
+                return this.enable;
+            }
+            set {
+                if(value && value != enable) {
+                    Hook();
+                } else if(!value && value != enable){
+                    Unhook();
+                }
+                enable = value;
+            }
+        }
+
+        
+
+        /// <summary>
+        /// 鍵盤按下後觸發的事件
+        /// </summary>
+        public event KeyEventHandler KeyDown;
+        /// <summary>
+        /// 鍵盤按鍵鬆開時的事件
+        /// </summary>
+        public event KeyEventHandler KeyUp;
+
+        
+        public GlobalKeyboardHook(bool NewAndHook = false) {
+            llkh = new LLKeyboardHook(HookProc);//設定Handler
+            HookedKeys = new List<Keys>();//初始化集合
+            if(NewAndHook) {//如果允許建構物件後立刻開始監聽
+                Hook();
+            }
+        }
+
+        public GlobalKeyboardHook(bool NewAndHook ,params Keys[] HookKeys):this() {
+            this.HookedKeys.AddRange(HookKeys);
+            if(NewAndHook) {
+                Hook();
+            }
+        }
+
+        public GlobalKeyboardHook(bool NewAndHook ,bool HookAllKeys):this() {
+            Keys[] HookKeys = Enum.GetValues(typeof(Keys)).Cast<Keys>().ToArray();
+            this.HookedKeys.AddRange(HookKeys);
+            if(NewAndHook) {
+                Hook();
+            }
+        }
+
+        ~GlobalKeyboardHook() { Unhook(); }//解構子，釋放監聽掛勾
+
+        /// <summary>
+        /// 啟用掛勾
+        /// </summary>
+        public void Hook() {
+            if(enable) {//已經啟用則先關閉在啟用
+                Unhook();
+            }
+            IntPtr hInstance = LoadLibrary("User32");//將監聽目標掛在User32，達到全域效果
+            HookTarget = SetWindowsHookEx(WH_KEYBOARD_LL ,llkh ,hInstance ,0);
+            enable = true;
+        }
+
+        /// <summary>
+        /// 停用掛勾
+        /// </summary>
+        public void Unhook() {
+            if(!enable) {//尚未啟用則不能關閉
+                return;
+            }
+            UnhookWindowsHookEx(HookTarget);
+            enable = false;
+        }
+
+        private int HookProc(int Code ,int wParam ,ref keyBoardHookStruct lParam) {
+            if(Code >= 0) {
+                Keys key = (Keys)lParam.vkCode;
+                if(HookedKeys.Contains(key)) {
+                    KeyEventArgs kArg = new KeyEventArgs(key);
+                    if((wParam == WM_KEYDOWN || wParam == WM_SYSKEYDOWN) && (KeyDown != null))
+                        KeyDown(this ,kArg);
+                    else if((wParam == WM_KEYUP || wParam == WM_SYSKEYUP) && (KeyUp != null))
+                        KeyUp(this ,kArg);
+                    if(kArg.Handled)
+                        return 1;
+                }
+            }
+            return CallNextHookEx(HookTarget ,Code ,wParam ,ref lParam);
+        }
+    }
+}
